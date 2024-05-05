982edf87a91de23b4a2dcd990a333df88652a160
Lê Gia Phú <phugiale2912@gmail.com>
2024-05-05 13:07:37 +0700

    feat: add npm packges

--- src/app.js
+++ src/app.js
@@ -1,2 +1,20 @@
-console.log("hello world");
-console.log("hello world");
+// Package Requirements:
+require("dotenv").config();
+const express = require("express");
+// Custom Utils:
+// Enviroment Variables:
+const port = process.env.BE_PORT;
+// Initialize Application:
+const app = express();
+// Application Use:
+//
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    code: 1,
+    message: "Default branch",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`> Website is running at http://localhost:${port}`);
+});
