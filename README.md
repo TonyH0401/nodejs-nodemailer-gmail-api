e2763e42ea1d80ec10b61a869920b850090db617
Lê Gia Phú <phugiale2912@gmail.com>
2024-05-06 09:53:20 +0700

    feat: add npm packages 2

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-updating....
+updating...
+
+You will need .env:
+    - BE_PORT
+    
+
--- src/app.js
+++ src/app.js
@@ -1,20 +1,34 @@
 // Package Requirements:
 require("dotenv").config();
 const express = require("express");
-// Custom Utils:
+const bodyParser = require("body-parser");
+const createError = require("http-errors");
+const cors = require("cors");
+const helmet = require("helmet");
+const path = require("path");
+const chalk = require("chalk");
+// Custom Utils (Requirements):
 // Enviroment Variables:
-const port = process.env.BE_PORT;
-// Initialize Application:
+const port = process.env.BE_PORT || 8080;
+// Initialize NodeJS ExpressJS Application:
 const app = express();
 // Application Use:
-//
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+// Default Routers:
 app.get("/", (req, res) => {
   return res.status(200).json({
     code: 1,
     message: "Default branch",
   });
 });
-
+// API Routers:
+// Default Error Handling:
+// Initialize Server:
 app.listen(port, () => {
   console.log(`> Website is running at http://localhost:${port}`);
 });
