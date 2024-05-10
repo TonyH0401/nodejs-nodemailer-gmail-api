de5ec59ea310a40546389395ce32de55bf0d90a4
Lê Gia Phú <phugiale2912@gmail.com>
2024-05-10 20:48:58 +0700

    style: add emoji to denied routes

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
 // API Routers:
 // Default Error Handling:
 app.use((req, res, next) => {
-  next(createError(404, "This directory does not exist! yet"));
+  next(createError(404, "This directory does not exist 🚫! yet"));
 });
 // Initialize Server:
 app.listen(port, () => {
