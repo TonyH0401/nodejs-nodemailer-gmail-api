3aec0411c0dda2ccea84fc61cbf72f6bc187e153
Lê Gia Phú <phugiale2912@gmail.com>
2024-05-14 10:16:02 +0700

    docs: add notes for func

--- src/utils/requestLogger.js
+++ src/utils/requestLogger.js
@@ -3,11 +3,13 @@ const morgan = require("morgan");
 // Request Logger Modes:
 const reqLoggerTiny = morgan("tiny ");
 const reqLoggerDev = morgan("dev");
+/* this mode accepts error request only, they are requests that have a code above 400. */
 const reqLoggerDevErr = morgan("dev", {
   skip: function (req, res) {
     return res.statusCode < 400;
   },
 });
+/* this mode returns method, url, status and response time it is custome made. */
 const reqLoggerCustom1 = morgan(function (tokens, req, res) {
   return [
     tokens.method(req, res),
