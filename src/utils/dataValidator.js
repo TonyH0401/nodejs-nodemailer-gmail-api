ef4da3ce26ece5ee42d84506e3b6a0c1ac6cb1fb
Lê Gia Phú <phugiale2912@gmail.com>
2024-05-20 14:45:09 +0700

    feat: add email validation method

--- src/utils/dataValidator.js
+++ src/utils/dataValidator.js
@@ -1,5 +1,15 @@
 // Package Requirements:
+const validator = require("validator");
 // Custom Utils (Requirements):
 // Constant Declarations:
+// Data Validators:
+/* validate array of email addresses */
+/**
+ * @param {string[]} emailArray - An array of email addresses to be validated.
+ * @returns {boolean} - Returns true if all email addresses are valid, otherwise returns false.
+ */
+function validateEmails(emailArray) {
+  return emailArray.every((email) => validator.isEmail(email));
+}
 // Exports:
-module.exports = {};
+module.exports = { validateEmails };
